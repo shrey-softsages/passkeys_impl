a63bfc57a82dea81d5953f38f29922445146ccc7
shrey-softsages <shrey.softsages@gmail.com>
2024-01-17 18:29:25 +0530

    project commit

---
+++ lib/main.dart
@@ -0,0 +1,220 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:passkeys/passkey_auth.dart';
+// import 'package:passkeys_example/relying_party_server.dart';
+import 'package:passkeys_impl/relying_party_server.dart';
+
+final relyingPartServer = PersonalRelyingPartyServer();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  relyingPartServer.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(
+        useMaterial3: false,
+        colorScheme: const ColorScheme(
+          brightness: Brightness.light,
+          primary: Color(0xFF1953ff),
+          onPrimary: Colors.white,
+          secondary: Colors.white,
+          onSecondary: Colors.black,
+          error: Colors.redAccent,
+          onError: Colors.white,
+          background: Color(0xFF1953ff),
+          onBackground: Colors.white,
+          surface: Color(0xFF1953ff),
+          onSurface: Color(0xFF1953ff),
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({super.key}) : _auth = PasskeyAuth(relyingPartServer);
+
+  final PasskeyAuth<RpRequest, RpResponse> _auth;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+enum PageMode { registration, login, loggedIn }
+
+class _HomePageState extends State<HomePage> {
+  final _emailController = TextEditingController();
+  PageMode _pageMode = PageMode.registration;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Passkeys Example')),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: 5,
+                ),
+                child: Text(
+                  'Tired of passwords?',
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 50, top: 10, bottom: 10),
+                child: Text(
+                  'Sign in using your biometrics like fingerprint or face.',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 4),
+                child: TextField(
+                  autofillHints: const [AutofillHints.username],
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'email address',
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: _onclick,
+                  child: Text(_buttonText()),
+                ),
+              ),
+              const SizedBox(height: 16),
+              _drawSubLine()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  String _buttonText() {
+    if (_pageMode == PageMode.registration) {
+      return 'sign up';
+    } else if (_pageMode == PageMode.login) {
+      return 'sign in';
+    } else {
+      return 'logout';
+    }
+  }
+
+  Widget _drawSubLine() {
+    if (_pageMode == PageMode.registration) {
+      return RichText(
+        text: TextSpan(
+          children: [
+            const TextSpan(
+              text: 'Already have an account? ',
+              style: TextStyle(color: Colors.black),
+            ),
+            TextSpan(
+              text: 'Sign in',
+              style: TextStyle(color: Theme.of(context).primaryColor),
+              recognizer: TapGestureRecognizer()
+                ..onTap = () async {
+                  setState(() {
+                    _pageMode = PageMode.login;
+                  });
+                },
+            )
+          ],
+        ),
+      );
+    } else if (_pageMode == PageMode.login) {
+      return RichText(
+        text: TextSpan(
+          children: [
+            const TextSpan(
+              text: 'First time here? ',
+              style: TextStyle(color: Colors.black),
+            ),
+            TextSpan(
+              text: 'Sign up',
+              style: TextStyle(color: Theme.of(context).primaryColor),
+              recognizer: TapGestureRecognizer()
+                ..onTap = () async {
+                  setState(() {
+                    _pageMode = PageMode.registration;
+                  });
+                },
+            )
+          ],
+        ),
+      );
+    } else {
+      return const Text('You are currently logged in.');
+    }
+  }
+
+  Future<void> _onclick() async {
+    final email = _emailController.value.text;
+
+    try {
+      if (_pageMode == PageMode.registration) {
+        bool isSupported = await widget._auth.isSupported();
+        if (isSupported) {
+          print("Supporteddddddddddd");
+        } else {
+          print("Not supporteddddddddddddddddd");
+        }
+        final response =
+            await widget._auth.registerWithEmail(RpRequest(email: email));
+        print("Authenticatedddddd");
+        if (response != null) {
+          setState(() {
+            _pageMode = PageMode.loggedIn;
+          });
+        }
+      } else if (_pageMode == PageMode.login) {
+        final response =
+            await widget._auth.authenticateWithEmail(RpRequest(email: email));
+
+        if (response != null) {
+          setState(() {
+            _pageMode = PageMode.loggedIn;
+          });
+        }
+      } else {
+        setState(() {
+          _pageMode = PageMode.login;
+        });
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: Theme.of(context).primaryColor,
+          content: Text(e.toString()),
+          duration: const Duration(seconds: 10),
+        ),
+      );
+    }
+  }
+}
---
+++ lib/relying_party_server.dart
@@ -0,0 +1,118 @@
+import 'dart:convert';
+
+import 'dart:io';
+
+import 'package:cookie_jar/cookie_jar.dart';
+import 'package:passkeys/relying_party_server/relying_party_server.dart';
+import 'package:passkeys/relying_party_server/types/authentication.dart';
+import 'package:passkeys/relying_party_server/types/registration.dart';
+import 'package:http/http.dart' as https;
+
+class PersonalRelyingPartyServer
+    extends RelyingPartyServer<RpRequest, RpResponse> {
+  final client = https.Client();
+  String? cookie;
+  void init() {}
+
+  @override
+  Future<RpResponse> completeAuthenticate(
+      AuthenticationCompleteRequest request) async {
+    return RpResponse(success: true);
+  }
+
+  @override
+  Future<AuthenticationInitResponse> initAuthenticate(RpRequest request) async {
+    Map<String, dynamic> body = {"username": request.email};
+    final resposne = await client.post(
+        Uri.parse("https://e063-115-246-26-148.ngrok-free.app/login"),
+        body: body);
+    Map<String, dynamic> credentials = {};
+    if (resposne.statusCode == 200) {
+      credentials = json.decode(resposne.body);
+      print(credentials);
+    } else {
+      print("FAILLLLLLLLLLLLLL");
+    }
+    return AuthenticationInitResponse(
+        rpId: credentials['publicKey']['rpId'],
+        challenge: credentials['publicKey']['challenge']);
+  }
+
+  @override
+  Future<RegistrationInitResponse> initRegister(RpRequest request) async {
+    Map<String, String> body = {
+      "username": request.email,
+      "display": request.email
+    };
+    final response = await client.post(
+        Uri.parse("https://e063-115-246-26-148.ngrok-free.app/register"),
+        body: body);
+    cookie = response.headers['set-cookie'];
+    Map<String, dynamic> credentialCreationRequestoptions = {};
+    if (response.statusCode == 200) {
+      credentialCreationRequestoptions = json.decode(response.body);
+      credentialCreationRequestoptions['publicKey']['origin'] =
+          "https://e063-115-246-26-148.ngrok-free.app";
+      print(credentialCreationRequestoptions);
+    } else {
+      print("FAILLLLLLLLLLLLLL");
+    }
+    // List<PubKeyCredParam> pubKeyCredParams =
+    //     credentialCreationRequestoptions['publicKey']['pubKeyCredParams']
+    //         .map((credparams) =>
+    //             PubKeyCredParam(credparams['alg'], credparams['type']))
+    //         .toList();
+
+    return RegistrationInitResponse(
+      RelyingParty(credentialCreationRequestoptions['publicKey']['rp']['name'],
+          credentialCreationRequestoptions['publicKey']['rp']['id']),
+      User(
+          credentialCreationRequestoptions['publicKey']['user']['displayName'],
+          credentialCreationRequestoptions['publicKey']['user']['name'],
+          credentialCreationRequestoptions['publicKey']['user']['id']),
+      credentialCreationRequestoptions['publicKey']['challenge'],
+      AuthenticatorSelection("platform", false, "preferred", "preferred"),
+      timeout: 60000,
+    );
+  }
+
+  @override
+  Future<RpResponse> completeRegister(
+      RegistrationCompleteRequest request) async {
+    // print(request.id + "\n" + request.clientDataJSON + "\n" + request.rawId);
+    Map<String, dynamic> body = {
+      "username": "shrey12",
+      "credential": jsonEncode({
+        "id": request.id,
+        "response": {
+          "clientDataJSON": request.clientDataJSON,
+          "attestationObject": request.attestationObject
+        },
+        "type": "public-key",
+        "clientExtensionResults": {
+          "credProps": {"rk": true}
+        }
+      }),
+      "credname": "shrey12"
+    };
+    final resposne = await client.post(
+        Uri.parse("https://e063-115-246-26-148.ngrok-free.app/finishauth"),
+        body: body,
+        headers: {'Cookie': cookie!});
+    print(resposne.body);
+    print(resposne.headers['set-cookie']);
+    return RpResponse(success: true);
+  }
+}
+
+class RpRequest {
+  const RpRequest({required this.email});
+
+  final String email;
+}
+
+class RpResponse {
+  const RpResponse({required this.success});
+
+  final bool success;
+}
